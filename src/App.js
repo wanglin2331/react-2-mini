9077721fb64db2c37d39c589b2a6b8ad7eb126bb
Lin Wang <lin.wang@healthcatalyst.com>
2018-01-30 13:15:38 -0700

    all done

--- src/App.js
+++ src/App.js
@@ -8,27 +8,55 @@ import FamilyChanger from './components/FamilyChanger';
 import TextContainer from './components/TextContainer';
 
 class App extends Component {
-  // constructor
+   constructor(props){
+     super(props)
 
-  // updateColor
+    this.state = {
+      fontColor: 'black',
+      fontSize: 12,
+      fontFamily: 'monospace',
+      allowEdit: 'true'
+    }
 
-  // updateSize
+    this.updateColor = this.updateColor.bind(this);
+    this.updateSize = this.updateSize.bind(this);
+    this.updateFamily = this.updateFamily.bind(this);
+    this.updateEditStatus = this.updateEditStatus.bind(this);
+   }
 
-  // updateFamily
+  updateColor(val) {
+    this.setState({fontColor: val}) 
+  }
 
-  // updateEditStatus
+   // updateColor = (val)=>{
+  //   this.setState({fontColor: val}) 
+  // }            you can do this as well
+
+   updateSize(val) {
+    this.setState({fontSize: parseInt(val)}) 
+  }
+
+   updateFamily(val) {
+    this.setState({fontFamily: val}) 
+  }
+  
+   updateEditStatus(val) {
+    this.setState({allowEdit: val}) 
+  }
 
   render() {
     return (
       <div>
         <div className="headerBar">
-          { /* Render EditToggle */ }
-          { /* Render ColorChanger */ }
-          { /* Render SizeChanger */ }
-          { /* Render FamilyChanger */ }
+          <EditToggle update = {this.updateEditStatus}/>
+          <ColorChanger update = {this.updateColor} allowEdit={this.state.allowEdit}/>
+          <SizeChanger update = {this.updateSize} allowEdit={this.state.allowEdit}/>
+          <FamilyChanger update = {this.updateFamily} allowEdit={this.state.allowEdit}/>
         </div>
         <div className="textArea">
-          { /* Render TextContainer */ }
+          <TextContainer  fontColor = {this.state.fontColor}
+                          fontSize = {this.state.fontSize}
+                          fontFamily = {this.state.fontFamily} />
         </div>
       </div>
     )
